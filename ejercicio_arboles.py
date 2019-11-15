e8c6d833052bc72d95f934a5650a140d70aa663f
Xania Mendoza <31690927+xaniamdza@users.noreply.github.com>
2019-11-14 21:51:14 -0600

    Add files via upload

---
+++ ejercicio_arboles.py
@@ -0,0 +1,8 @@
+from arboles_binarios import NodoArbol
+
+def main():
+
+    root = NodoArbol('10', NodoArbol('20', None, NodoArbol('30', None, NodoArbol(None, '40', NodoArbol('50', '60', NodoArbol(None, '70', NodoArbol('80', None)))))))
+   
+
+main()    
